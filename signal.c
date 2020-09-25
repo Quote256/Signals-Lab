c4488974935f8434f70f17633579edc326c38bfa
Bradon Thymes <bmt.wcpa@gmail.com>
2020-09-24 23:54:54 -0400

    Worked on the main signal part of the program

--- signal.c
+++ signal.c
@@ -4,9 +4,12 @@
 #include <signal.h>
 #include <unistd.h>
 
+int t = 0;
+
 void handler(int signum)
 { //signal handler
   printf("Hello World!\n");
+  t=1;
   exit(1); //exit after printing
 }
 
@@ -14,6 +17,11 @@ int main(int argc, char * argv[])
 {
   signal(SIGALRM,handler); //register handler to handle SIGALRM
   alarm(1); //Schedule a SIGALRM for 1 second
-  while(1); //busy wait for signal to be delivered
+  while(1){
+      while(t==0){
+          printf("Turing was right!!\n");
+          t = 0;
+      }
+  }
   return 0; //never reached
 }
